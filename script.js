2e358b763f41fb0ccfca4eab826931c25d09241d
valentinaaranggo <valea28@gmail.com>
2023-01-24 09:58:01 -0500

    changed the question 1 function to an object variable which includes all the questions and its properties

--- script.js
+++ script.js
@@ -3,44 +3,60 @@ var  mainE1=document.getElementById("main");
 var headerE1=document.querySelector(".header");
 var subHeaderE1=document.querySelector(".sub-header");
 var olE1=document.querySelector(".answer-choices");
+var secondsLeft=100;
+
+var questions= [
+    {Question:"Commonly used data types DO NOT Include: ",
+    Choices:["String","Boolean","Numbers", "Alerts"],
+    Answer: "Alerts"
+},
+   {Question:"The condition in an if/else statment is enclosed within ______",
+    Choices:["1.quotes","2.curly brackets","3.parentheses","4.square brackets"],
+    Answer:"3.Parentheses"},
+    
+    {Question:"Arrays in JavaScript can be used to store _____",
+    Choices:["1.numbers and strings","2.other array","3.booleans","4.all of the above"],
+    Answer:"4.all of the above"}
+
+]
 
 
 function startChallenge(){
+    timeE1.textContent="time: 0"
     headerE1.textContent= "Coding Quiz Challenge";
     subHeaderE1.textContent= "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by 15 seconds!";
     var startBtn= document.createElement('button');
     startBtn.innerText= "start Quiz";
     subHeaderE1.appendChild(startBtn);
-    startBtn.addEventListener('click', function(){
-        question1();
+    startBtn.addEventListener('click', function(){ 
+        setTime();
+        playchallenge()
+
     })
 
-
+function playchallenge(){
+    
 
 }
-function question1(){
-    headerE1.textContent="commonly used data types DO NOT include:";
-    subHeaderE1.textContent=""
-    var liE1= document.createElement('li');
-    var liE2= document.createElement('li');
-    var liE3= document.createElement('li');
-    var liE4= document.createElement('li');
-    olE1.appendChild(liE1);
-    olE1.appendChild(liE2);
-    olE1.appendChild(liE3);
-    olE1.appendChild(liE4);
-    var choiceOne=document.createElement('button');
-    var choiceTwo=document.createElement('button');
-    var choiceThree=document.createElement('button');
-    var choiceFour=document.createElement('button');
-    liE1.appendChild(choiceOne);
-    liE2.appendChild(choiceTwo);
-    liE3.appendChild(choiceThree);
-    liE4.appendChild(choiceFour);
-    choiceOne.textContent="strings";
-    choiceTwo.textContent="boolean";
-    choiceThree.textContent="numbers";
-    choiceFour.textContent="alerts";
+
 }
+function setTime(){
+    var timerInterval=setInterval(function(){
+        secondsLeft--;
+        timeE1.textContent='Time left: '+secondsLeft;
+    
+    if (secondsLeft==0){
+        clearInterval(timerInterval);
+        console.log("You Lost")
+
+    }
+
+
+},1000)
+    
+
+}
+
+
 
 startChallenge();
